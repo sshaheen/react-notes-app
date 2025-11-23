8293d8fb8582197a0a706e8530a20a4b24c52d8c
Salem Shaheen <luma@Salems-MacBook-Pro.local>
2025-11-23 13:25:56 -0500

    Add Note Component and use Prop Drilling

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -5,12 +5,6 @@ const NoteList = ({ notes, deleteNote }) => {
     return <p className='text-center text-gray-500'>No Notes Yet</p>;
   }
 
-  const borderColors = {
-    Low: 'border-green-500',
-    Medium: 'border-yellow-500',
-    High: 'border-red-500',
-  };
-
   return (
     <div className='space-y-4'>
       {notes.map((note) => (
