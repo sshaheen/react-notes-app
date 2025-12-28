426b2b59bdaf14cc15716974044d9aa6b0d5ed3d
Salem Shaheen <luma@Salems-MacBook-Pro.local>
2025-12-28 18:33:48 -0500

    Learn and apply useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList';
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    return notes || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   const deleteNote = (id) => {
     const confirmDelete = window.confirm(
@@ -11,7 +18,7 @@ const App = () => {
     );
 
     if (confirmDelete) {
-      setNotes(notes.filter((note) => note.id != id));
+      setNotes(notes.filter((note) => note.id !== id));
     }
   };
 
